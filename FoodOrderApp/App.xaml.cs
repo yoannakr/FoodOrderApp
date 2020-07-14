4e599ce1e863e14f69cbaf7155c5b7d45e8add37
yoannakr <jkrmilkova@gmail.com>
2020-07-14 19:01:15 +0300

    Add project files.

---
+++ FoodOrderApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using FoodOrderApp.Services;
+using FoodOrderApp.Views;
+
+namespace FoodOrderApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ FoodOrderApp/Models/HomeMenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodOrderApp.Models
+{
+    public enum MenuItemType
+    {
+        Foods,
+        About
+    }
+    public class HomeMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
